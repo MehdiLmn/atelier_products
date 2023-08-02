79df30a0f4636ad1fb37904739e5894aac22ced9
root <MehdiLmn>
2023-08-02 11:54:34 +0200

    commit 2

--- functions/tva.php
+++ functions/tva.php
@@ -1,12 +1,18 @@
 <?php
+
 /**
- * Undocumented function
+ * Calculates price with taxes for the given price and given tax
  *
- * @param float $price
- * @param float $tva (=1.2)
- * @return float
+ * @param float $price base price (without tax)
+ * @param float $vat tax rate (e.g. 20% -> 1.2)
+ * @return float Full tax price
  */
-function getTotalPrice(float $price, float $tva): float
+function getTotalPrice(float $price, float $vat): float
 {
-    return $price * $tva;
+  if ($vat < 1) {
+    echo "Tax rate must be greater than 1";
+    exit;
+  }
+
+  return $price * $vat;
 }
