ed3e909495e630d923f85f248884288e1b2962b4
root <MehdiLmn>
2023-08-02 16:38:30 +0200

    commit 43

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,10 +1,10 @@
 <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">Les produits</a>
-    <a class="navbar-brand" href="index.php">Test</a>
+    <a class="navbar-brand" href="search.php">Recherche</a>
     <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
+      <input class="form-control me-2" type="search" placeholder="Recherche" aria-label="Recherche">
+      <button class="btn btn-outline-success" type="submit">Recherche</button>
     </form>
   </div>
 </nav>
